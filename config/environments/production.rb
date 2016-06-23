d1501409def3a06789e88fe745395dbcdc455be3
Yuzhou Liu <yuzhoul.eecs@gmail.com>
2016-06-23 06:59:35 -0700

    mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,11 +20,11 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            'yuzhoul.eecs@gmail.com',
-    password:             'Change59478',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+   :port           => ENV['MAILGUN_SMTP_PORT'],
+   :address        => ENV['MAILGUN_SMTP_SERVER'],
+   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+   :domain         => 'myflix-yuzhoul.herokuapp.com', #eg: 'yourappname.herokuapp.com'
+   :authentication => :plain,
+ }
 end
