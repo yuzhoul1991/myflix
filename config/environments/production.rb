6e300286b4827187acee2f4ebecf608353740b15
Yuzhou Liu <yuzhoul.eecs@gmail.com>
2016-06-23 07:07:24 -0700

    add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,7 @@ Myflix::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'myflix-yuzhoul.herokuapp.com' }
   config.action_mailer.smtp_settings = {
    :port           => ENV['MAILGUN_SMTP_PORT'],
    :address        => ENV['MAILGUN_SMTP_SERVER'],
